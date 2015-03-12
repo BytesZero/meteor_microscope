ed8c6b04c57ee60ffe49730f810ed9ac9b5d4abf
zhengsl <zhengsl@tongxinyiliao.com>
2015-03-12 11:14:55 +0800

    把数据库集合连接到 `postsList` 模板上

--- microscope/client/templates/posts/posts_list.js
+++ microscope/client/templates/posts/posts_list.js
@@ -1,20 +1,8 @@
 /**
  * Created by fitark on 15-3-12.
  */
-var postsData = [
-    {
-        title: 'Introducing Telescope',
-        url: 'http://sachagreif.com/introducing-telescope/'
-    },
-    {
-        title: 'Meteor',
-        url: 'http://meteor.com'
-    },
-    {
-        title: 'The Meteor Book',
-        url: 'http://themeteorbook.com'
-    }
-];
 Template.postsList.helpers({
-    posts: postsData
+    posts: function(){
+        return Posts.find();
+    }
 });
---
+++ microscope/lib/collections/posts.js
@@ -0,0 +1,4 @@
+/**
+ * Created by fitark on 15-3-12.
+ */
+Posts = new Mongo.Collection('posts');

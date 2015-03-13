0df3e7d43474db6d182e626dd7c9434b3e6acb0f
zhengsl <zhengsl@tongxinyiliao.com>
2015-03-13 13:13:52 +0800

    当没有登录的时候拒绝访问帖子创建页面

---
+++ microscope/client/templates/posts/post_submit.js
@@ -0,0 +1,16 @@
+/**
+ * Created by fitark on 15-3-13.
+ */
+Template.postSubmit.events({
+    'submit form': function(e) {
+        e.preventDefault();
+
+        var post = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val()
+        };
+
+        post._id = Posts.insert(post);
+        Router.go('postsList', post);
+    }
+});
--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -2,3 +2,9 @@
  * Created by fitark on 15-3-12.
  */
 Posts = new Mongo.Collection('posts');
+Posts.allow({
+    insert: function(userId, doc) {
+        // 只允许登录用户添加帖子
+        return !! userId;
+    }
+});
